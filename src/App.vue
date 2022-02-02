a1b7285aca90a0dada4efff1fb4b00a9d4138c9f
ella <ellakaminski29@gmail.com>
2022-02-02 17:37:09 +0200

    Test button

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <div id="app" v-if="fronteggLoaded">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <p>
-      Logged in as
-      {{ this.authState.user ? this.authState.user.email : "Not logged in" }}
-    </p>
+  <div id="app">
+    <div v-if="fronteggLoaded">
+      <img alt="Vue logo" src="./assets/logo.png" />
+      <p>
+        Logged in as
+        {{ this.authState.user ? this.authState.user.email : "Not logged in" }}
+      </p>
+    </div>
   </div>
 </template>
 
